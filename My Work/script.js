355c92ab57d91c904af7f6e175dc449a81e662d5
AhmedSameh19 <ahmed.sameh7433@gmail.com>
2022-08-21 21:07:16 +0200

    l

--- My Work/script.js
+++ My Work/script.js
@@ -1,24 +1,9 @@
-import gsap from "./gsap-core.js";
-import { Timeline } from "gsap/gsap-core"
-export { TweenLite, TimelineMax, TimelineLite, Power0, Power1, Power2, Power3, Power4, Linear, Quad, Cubic, Quart, Quint, Strong, Elastic, Back, SteppedEase, Bounce, Sine, Expo, Circ, wrap, wrapYoyo, distribute, random, snap, normalize, getUnit, clamp, splitColor, toArray, mapRange, pipe, unitize, interpolate, shuffle, selector } from "./gsap-core.js";
-const img= document.getElementsByTagName("H1")
-var t1=new gsap.Timeline({defaults:{duration:2 , ease:"power3.inOut"}});
-t1.fromTo(img,{opacity:'0'},{opacity:'1'})
-
-
-
-
-
-
 document.getElementById("ham").addEventListener("click",function(){
     
     document.getElementById("ham").style.display="none";
     document.getElementById("back").style.display="flex";
     document.getElementById("list").style.display="flex";
-    document.getElementById("close").style.display="block";
-
-
-    
+    document.getElementById("close").style.display="block"; 
 })
 document.getElementById("close").addEventListener("click",function(){
     document.getElementById("ham").style.display="block";
